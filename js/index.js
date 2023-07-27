27eb0788f1b549b508283e1552487200de85f9a0
OrionFE <102513015+OrionFE@users.noreply.github.com>
2023-07-27 13:36:30 +0200

    add no recipe found

--- js/index.js
+++ js/index.js
@@ -17,80 +17,89 @@ function displayRecipe (arrayRecipe) {
 
     document.querySelector('.nb-recipe').innerText = recipesToShow.length
 
-    recipesToShow.map((recipe => {
+    if(recipesToShow.length === 0){
+        console.log('test')
+        const noRecipe = document.createElement('p')
+        noRecipe.innerText = 'Aucune recette correspondante à la recherche'
+        recipesContainer.appendChild(noRecipe)
+    } else {
+        recipesToShow.map((recipe => {
 
-        const {name , image , description , time , ingredients } = recipe
+            const {name , image , description , time , ingredients } = recipe
 
-        const card = document.createElement('div')
-        card.classList.add('card')
+            const card = document.createElement('div')
+            card.classList.add('card')
 
-        const img = document.createElement('img')
-        img.src = `./img/${image}`
-        img.classList.add('img-card')
-        card.appendChild(img)
+            const img = document.createElement('img')
+            img.src = `./img/${image}`
+            img.classList.add('img-card')
+            card.appendChild(img)
 
-        const timer = document.createElement('p')
-        timer.classList.add('timer')
-        timer.innerText = time + 'min'
-        card.appendChild(timer)
+            const timer = document.createElement('p')
+            timer.classList.add('timer')
+            timer.innerText = time + 'min'
+            card.appendChild(timer)
 
-        const cardContent = document.createElement('div')
-        cardContent.classList.add('card-content')
+            const cardContent = document.createElement('div')
+            cardContent.classList.add('card-content')
 
-        const title = document.createElement('h3')
-        title.innerText = name
-        cardContent.appendChild(title)
+            const title = document.createElement('h3')
+            title.innerText = name
+            cardContent.appendChild(title)
 
-        const descriptionDiv = document.createElement('div')
-        const descriptionTitle = document.createElement('h4')
-        descriptionTitle.innerText = "RECETTE"
-        descriptionDiv.appendChild(descriptionTitle)
-        const descriptionContent = document.createElement('p')
-        descriptionContent.classList.add('description-content')
-        descriptionContent.innerText = description
-        descriptionDiv.appendChild(descriptionContent)
-        cardContent.appendChild(descriptionDiv)
+            const descriptionDiv = document.createElement('div')
+            const descriptionTitle = document.createElement('h4')
+            descriptionTitle.innerText = "RECETTE"
+            descriptionDiv.appendChild(descriptionTitle)
+            const descriptionContent = document.createElement('p')
+            descriptionContent.classList.add('description-content')
+            descriptionContent.innerText = description
+            descriptionDiv.appendChild(descriptionContent)
+            cardContent.appendChild(descriptionDiv)
 
-        const ingredientDiv = document.createElement('div')
-        const ingredientTitle = document.createElement('h4')
-        ingredientTitle.innerText = 'INGREDIENTS'
-        ingredientDiv.appendChild(ingredientTitle)
-        const ingredientList = document.createElement('ul')
+            const ingredientDiv = document.createElement('div')
+            const ingredientTitle = document.createElement('h4')
+            ingredientTitle.innerText = 'INGREDIENTS'
+            ingredientDiv.appendChild(ingredientTitle)
+            const ingredientList = document.createElement('ul')
 
-        ingredients.map((ing) => {
+            ingredients.map((ing) => {
 
-            const { ingredient , quantity , unit } = ing
+                const { ingredient , quantity , unit } = ing
 
-            const list = document.createElement('li')
-            list.classList.add('list-ingredient')
+                const list = document.createElement('li')
+                list.classList.add('list-ingredient')
 
-            let prefix = ''
-            unit ? prefix= unit : prefix = ''
+                let prefix = ''
+                unit ? prefix= unit : prefix = ''
 
-            let quantityFormat = ''
-            quantity ? quantityFormat= quantity : quantityFormat='-'
-
-
-            const ingredientName = document.createElement('p')
-            ingredientName.classList.add('ingredient-name')
-            ingredientName.innerText = ingredient
-            list.appendChild(ingredientName)
-
-            const ingredientAmount = document.createElement('p')
-            ingredientAmount.classList.add('ingredient-amount')
-            ingredientAmount.innerText = quantityFormat + ' ' + prefix
-            list.appendChild(ingredientAmount)
-
-            ingredientList.appendChild(list)
-        })
-
-        ingredientDiv.appendChild(ingredientList)
-        cardContent.appendChild(ingredientDiv)
-        card.appendChild(cardContent)
-        recipesContainer.appendChild(card)
+                let quantityFormat = ''
+                quantity ? quantityFormat= quantity : quantityFormat='-'
 
 
-    }))
+                const ingredientName = document.createElement('p')
+                ingredientName.classList.add('ingredient-name')
+                ingredientName.innerText = ingredient
+                list.appendChild(ingredientName)
+
+                const ingredientAmount = document.createElement('p')
+                ingredientAmount.classList.add('ingredient-amount')
+                ingredientAmount.innerText = quantityFormat + ' ' + prefix
+                list.appendChild(ingredientAmount)
+
+                ingredientList.appendChild(list)
+            })
+
+            ingredientDiv.appendChild(ingredientList)
+            cardContent.appendChild(ingredientDiv)
+            card.appendChild(cardContent)
+            recipesContainer.appendChild(card)
+
+
+        }))
+    }
+
+
 
 }
 function searchBarEvent (e) {
